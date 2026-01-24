27a7024f3100e7d6a7efb22dce44033a586d86d1
Pheonix Coder 🐦‍🔥 <coderpheonix100@gmail.com>
2026-01-24 04:58:25 +0000

    string: group anagrams

---
+++ src/string/GroupAnagram.java
@@ -0,0 +1,33 @@
+package string;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class GroupAnagram {
+    public static void main(String[] args) {
+        String[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+        List<List<String>> res = groupAnagrams(strs);
+
+        System.out.println(res);
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+
+        for (String s : strs) {
+            int[] count = new int[26];
+
+            for (char c : s.toCharArray()) 
+                count[c - 'a']++;
+
+            String key = Arrays.toString(count);
+            res.putIfAbsent(key, new ArrayList<>());
+            res.get(key).add(s);
+        }
+
+        return new ArrayList<>(res.values());
+    }
+}
